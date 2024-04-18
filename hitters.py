9eac639ad333cad28d79b371c081f396c6048919
tmking2002 <tmking4@ncsu.edu>
2024-04-17 22:31:34 -0400

    Update hitters.py

--- hitters.py
+++ hitters.py
@@ -53,26 +53,32 @@ upd_data['pitch'] = np.where(upd_data['RelSpeed'] < upd_data['max_RelSpeed'] - 1
 
 yakker = upd_data.copy()
 
-yakker['pitch'] = np.select([yakker['pitch'] == "Riseball" & yakker['InducedVertBreak'] > 5,
+yakker['pitch'] = np.select([(yakker['pitch'] == "Riseball") & (yakker['InducedVertBreak'] > 5),
                              yakker['pitch'] == "Riseball",
-                             yakker['pitch'] == "Dropball" & yakker['InducedVertBreak'] < -8,
+                             (yakker['pitch'] == "Dropball") & (yakker['InducedVertBreak'] < -8),
                              yakker['pitch'] == "Dropball",
-                             yakker['pitch'] == "Curveball" & yakker['HorzBreak'] < -6,
+                             (yakker['pitch'] == "Curveball") & (yakker['HorzBreak'] < -6),
                              yakker['pitch'] == "Curveball",
                              yakker['pitch'] == "Changeup"],
                             ["Good Riseball", "Bad Riseball", "Good Dropball", "Bad Dropball", "Good Curveball", "Bad Curveball", "Changeup"],
                             default=np.nan)
 
-yakker.dropna(subset=['pitch'], inplace=True)
+yakker = yakker.dropna(subset=['pitch'])
+yakker = yakker[~yakker['pitch'].isnull()]
 
 st.sidebar.header('GT Hitters')
 
 hitter = st.sidebar.selectbox('Select Hitter', yakker['Batter'].unique())
 
-result = st.sidebar.selectbox('Select Result', ['All', 'Hit', 'Hard Hit', 'Soft Contact', 'Whiff'])
+result = st.sidebar.multiselect('Select Result', ['All', 'Hit', 'Hard Hit', 'Soft Contact', 'Whiff', 'StrikeCalled'])
+
+pitches = ['All', 'Good Riseball', 'Bad Riseball', 'Good Dropball', 'Bad Dropball', 'Good Curveball', 'Bad Curveball', 'Changeup']
+
+pitch = st.sidebar.multiselect('Select Pitch', pitches)
 
 yakker = yakker[yakker['Batter'] == hitter]
 yakker = yakker[(yakker['PlateLocSide'] > -2) & (yakker['PlateLocSide'] < 2) & (yakker['PlateLocHeight'] > 0) & (yakker['PlateLocHeight'] < 5)]
+yakker = yakker[(yakker['pitch'].isin(pitch)) | ('All' in pitch)]
 
 yakker['Result'] = yakker['PitchCall']
 
@@ -80,31 +86,51 @@ yakker['Result'] = yakker['PitchCall']
 yakker.loc[(yakker['Result'] == 'InPlay') & ((yakker['ExitSpeed'] > 67.5) | (yakker['PlayResult'] == 'Home Run')), 'Result'] = 'Hard Hit'
 yakker.loc[(yakker['Result'] == 'InPlay') & (yakker['ExitSpeed'] < 67.5), 'Result'] = 'Soft Contact'
 yakker.loc[(yakker['Result'] == 'StrikeSwinging'), 'Result'] = 'Whiff'
+yakker.loc[(yakker['Result'] == 'StrikeCalled'), 'Result'] = 'StrikeCalled'
 
 yakker['HardHit'] = yakker['Result'].apply(lambda x: 1 if x == 'Hard Hit' else 0)
 yakker['Hit'] = yakker['PlayResult'].apply(lambda x: 1 if x in ['Single', 'Double', 'Triple', 'HomeRun'] else 0)
 yakker['Whiff'] = yakker['PitchCall'].apply(lambda x: 1 if x == 'StrikeSwinging' else 0)
+yakker['StrikeCalled'] = yakker['PitchCall'].apply(lambda x: 1 if x == 'StrikeCalled' else 0)
 
 yakker['Exit Velo'] = round(yakker['ExitSpeed'], 1)
+yakker.loc[yakker['Exit Velo'].isna(), 'Exit Velo'] = ''
 
-if result != 'All':
-    if result == 'Hit':
-        yakker = yakker[yakker['Hit'] == 1]
-    elif result == 'Hard Hit':
-        yakker = yakker[yakker['HardHit'] == 1]
-    elif result == 'Soft Contact':
-        yakker = yakker[yakker['Result'] == 'Soft Contact']
-    elif result == 'Whiff':
-        yakker = yakker[yakker['Whiff'] == 1]
+yakker['Pitch Speed'] = round(yakker['RelSpeed'], 1)
+yakker.loc[yakker['Pitch Speed'].isna(), 'Pitch Speed'] = ''
+
+final_yakker = pd.DataFrame()
+
+if 'All' not in result:
+    if 'Hit' in result:
+        final_yakker = pd.concat([final_yakker, yakker[yakker['Hit'] == 1]])
+    if 'Hard Hit' in result:
+        final_yakker = pd.concat([final_yakker, yakker[yakker['HardHit'] == 1]])
+    if 'Soft Contact' in result:
+        final_yakker = pd.concat([final_yakker, yakker[(yakker['HardHit'] == 0 & ~(yakker['Exit Velo'].isna()))]])
+    if 'Whiff' in result:
+        final_yakker = pd.concat([final_yakker, yakker[yakker['Whiff'] == 1]])
+    if 'StrikeCalled' in result:
+        final_yakker = pd.concat([final_yakker, yakker[yakker['StrikeCalled'] == 1]])
+else:
+    final_yakker = yakker
+
+yakker = final_yakker.copy()
+
+if yakker.empty:
+    st.write('No data available for selected filters')
+    st.stop()
 
 title = alt.TitleParams(hitter, anchor='middle')
+
+yakker = yakker.rename(columns={'pitch': 'Pitch'})
 
 scatter = alt.Chart(yakker, title=title).mark_circle(size=100).encode(
     alt.X('PlateLocSide', axis=alt.Axis(labels=False, ticks=False, title='')),
     alt.Y('PlateLocHeight', axis=alt.Axis(labels=False, ticks=False, title='')),
-    tooltip=['Result', 'PlayResult', 'Exit Velo', 'pitch']
+    tooltip=['Pitcher', 'Game', 'Pitch', 'Result', 'Pitch Speed', 'Exit Velo']
 ).properties(
-    height=800
+    height=500
 )
 
 # Define the lines
@@ -164,5 +190,17 @@ horz_line_chart = alt.Chart(horz_line).mark_line(color='black', strokeDash=[5,5]
 # Combine scatter plot and lines
 combined_chart = scatter + k_zone_chart + batter_box_1_chart + batter_box_2_chart + vert_line_chart + horz_line_chart
 
-
 st.altair_chart(combined_chart)
+
+if len(yakker[yakker['PitchCall'].isin(['StrikeSwinging', 'Foul', 'InPlay'])]) == 0:
+    whiff_rate = 0
+else:
+    whiff_rate = round((len(yakker[yakker['Whiff'] == 1]) / len(yakker[yakker['PitchCall'].isin(['StrikeSwinging', 'Foul', 'InPlay'])])) * 100, 1)
+
+if len(yakker[yakker['Result'].isin(['Hard Hit', 'Soft Contact'])]) == 0:
+    hard_hit_rate = 0
+else:
+    hard_hit_rate = round((len(yakker[yakker['HardHit'] == 1]) / len(yakker[yakker['Result'].isin(['Hard Hit', 'Soft Contact'])])) * 100, 1)
+
+st.write(f'Whiff Rate: {whiff_rate}%')
+st.write(f'Hard Hit Rate: {hard_hit_rate}%')
